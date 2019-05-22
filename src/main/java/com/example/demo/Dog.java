d82a3d653a8bd7cf0ddb65cb7f8629cf518a68a8
arahman1970 <ar.nj.usa@gmail.com>
2019-05-22 10:42:35 -0400

    exer 3.01

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    // name must be required
+    @NotNull
+    private  String name;
+
+    @Size(min=1)
+    private  int age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
